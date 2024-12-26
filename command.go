ed711be8bde92b095dc60432e578dbb032cd4f1a
Argorn5940 <sh.05940@gmail.com>
2024-12-27 04:47:50 +0900

    listtaskcmd関数、HandleCommand関数作成

---
+++ command.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//ListTasksCmd、タスク一覧を表示するコマンドを処理
+func ListTasksCmd(tm *TaskManager) error {
+	//DisplayTaskeメソッドを呼び出してタスク一覧を表示
+	if err := tm.DisplayTasks(); err != nil {
+		return fmt.Errorf("タスク一覧の表示に失敗しました: %v", err)
+	}
+	return nil
+}
+
+//HandleCommand コマンドの処理
+func HandleCommand(command string, tm *TaskManager, args []string) error {
+	switch command {
+	case "-list":
+		return ListTasksCmd(tm)
+	case "-comp":
+		if len(args) < 1 {
+			return fmt.Errorf("タスクIDを指定してください")
+		}
+		taskID := args[0]
+		return CompleteTaskCmd(tm, taskID)
+	case "-add":
+		if len(args) < 1 {
+			return fmt.Errorf("タスクの説明を入力してください")
+		}
+		return AddTaskCmd(tm, args[0])
+	case "-del":
+		if len(args) < 1 {
+			return fmt.Errorf("削除するタスクIDを指定してください")
+		}
+		return DeleteTaskCmd(tm, args[0])
+	default:
+		return fmt.Errorf("無効なコマンドです: %s", command)
+	}
+}
